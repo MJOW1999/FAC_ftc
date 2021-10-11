ce3b8f0d43e3f9d4822aa74262d69ddfaaaaea9b
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-11 12:03:13 +0100

    Challenge 1: Cookies

--- Authentication_W3/cookie-auth/workshop/server.js
+++ Authentication_W3/cookie-auth/workshop/server.js
@@ -6,31 +6,60 @@ const cookieParser = require("cookie-parser");
 
 server.use(cookieParser());
 
-server.get("/example", (request, response) => {
-  response.cookie("hello", "this is my cookie", {
-    httpOnly: true,
-    maxAge: 1000 * 60, // 60,000ms (60s)
-    sameSite: "lax",
-  });
-  response.redirect("/");
-});
-
-// server.get("/", (request, response) => {
-//   const cookies = request.get("cookie");
-//   console.log(cookies);
-//   response.send("<h1>Hello</h1>");
+// server.get("/example", (request, response) => {
+//   response.cookie("hello", "this is my cookie", {
+//     httpOnly: true,
+//     maxAge: 1000 * 60, // 60,000ms (60s)
+//     sameSite: "lax",
+//   });
+//   response.redirect("/");
+//   // const userInfo = {
+//   //   id: 1,
+//   //   username: "oliverjam",
+//   //   admin: true,
+//   // };
+//   // response.cookie("user", userInfo, {
+//   //   httpOnly: true,
+//   //   maxAge: 1000 * 60,
+//   //   sameSite: "lax",
+//   // });
+//   // response.redirect("/");
 // });
+
 server.get("/", (request, response) => {
   console.log(request.cookies);
-  // ...
+  response.send("<h1>Hello</h1>");
 });
 
-server.get("/remove", (request, response) => {
-  response.clearCookie("hello");
+// server.get("/remove", (request, response) => {
+//   response.clearCookie("hello");
+//   response.redirect("/");
+// });
+
+server.get("/login", (request, response) => {
+  const userInfo = {
+    id: 1,
+    username: "MJOW1999",
+    admin: true,
+  };
+  response.cookie("user", userInfo, {
+    httpOnly: true,
+    maxAge: 1000 * 60,
+    sameSite: "lax",
+    signed: true,
+  });
+  response.redirect("/")
+});
+
+server.get("/logout", (request, response) => {
+  response.clearCookie("user");
   response.redirect("/");
 });
 
+let sessions = {};
 
+// later
+sessions["abcd"] = { id: 1, username: "oliverjam", admin: true };
 
 const PORT = process.env.PORT || 4000;
 
