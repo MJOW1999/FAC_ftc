1c8d584b1dc124c2be53db80a55e77b7b9ac2607
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-11 11:33:09 +0100

    Cookies in node and express

--- Authentication_W3/cookie-auth/workshop/server.js
+++ Authentication_W3/cookie-auth/workshop/server.js
@@ -2,10 +2,36 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (request, response) => {
-  response.send("<h1>Hello</h1>");
+const cookieParser = require("cookie-parser");
+
+server.use(cookieParser());
+
+server.get("/example", (request, response) => {
+  response.cookie("hello", "this is my cookie", {
+    httpOnly: true,
+    maxAge: 1000 * 60, // 60,000ms (60s)
+    sameSite: "lax",
+  });
+  response.redirect("/");
 });
 
-const PORT = process.env.PORT || 3000;
+// server.get("/", (request, response) => {
+//   const cookies = request.get("cookie");
+//   console.log(cookies);
+//   response.send("<h1>Hello</h1>");
+// });
+server.get("/", (request, response) => {
+  console.log(request.cookies);
+  // ...
+});
+
+server.get("/remove", (request, response) => {
+  response.clearCookie("hello");
+  response.redirect("/");
+});
+
+
+
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
