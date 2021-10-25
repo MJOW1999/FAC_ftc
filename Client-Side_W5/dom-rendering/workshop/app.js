45cc5c483cd6b8e513134e7d7b8e018aeb17c61a
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-25 12:20:24 +0100

    Challenge 3: inner html

--- Client-Side_W5/dom-rendering/workshop/app.js
+++ Client-Side_W5/dom-rendering/workshop/app.js
@@ -1,19 +1,36 @@
 import cats from "./dogs";
-import createEl from "./create-element";
 
-const allDogs = cats.map((dog) => {
-  const h1 = createEl("h1", {}, dog.name);
-  const img = createEl("img", {
-    src: dog.image,
-    alt: `Image of ${dog.name}`,
-    width: 500,
-    height: 300,
-  });
-  return createEl("li", { className: "card" }, h1, img);
+const allDog = cats.map((dog) => {
+  return html`
+    <li class="card">
+      <h1>${dog.name}</h1>
+      <img src="${dog.image}" alt="Image of ${dog.name}" />
+    </li>
+  `;
 });
 
-const title = createEl("h1", {}, "All the dogs");
+document.querySelector("#app").innerHTML = html`
+  <h1>All the dogs</h1>
+  <ul>
+    ${dogElements.join("\n")}
+  </ul>
+`;
 
-const list = createEl("ul", {}, ...allDogs);
+// Challenge 2
 
-document.querySelector("#app").append(title, list);
+// const allDogs = cats.map((dog) => {
+//   const h1 = createEl("h1", {}, dog.name);
+//   const img = createEl("img", {
+//     src: dog.image,
+//     alt: `Image of ${dog.name}`,
+//     width: 500,
+//     height: 300,
+//   });
+//   return createEl("li", { className: "card" }, h1, img);
+// });
+
+// const title = createEl("h1", {}, "All the dogs");
+
+// const list = createEl("ul", {}, ...allDogs);
+
+// document.querySelector("#app").append(title, list);
