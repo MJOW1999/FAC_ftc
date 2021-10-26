c05fea274c5fcd8992475a12be9ec30671eb917d
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-26 11:13:21 +0100

    Part 5: Modularise your components

--- Client-Side_W5/react-forms/workshop/App.jsx
+++ Client-Side_W5/react-forms/workshop/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import dishes from "../data";
+import PriceFilter from "./PriceFilter";
 
 // const categories = [
 //   "all",
@@ -20,47 +21,12 @@ function App() {
       <section className="filters">
         <h1>Burger Place</h1>
         <form>
-          <fieldset>
-          <legend>Price</legend>
-          <label htmlFor="min-price">
-            Min price
-            <input
-          type="range"
-          id="min-price"
-          min="0.5"
-          max="9"
-          step="0.25"
-          value={min}
-          onChange={(event) => setMin(event.target.value)}
-          />
-          </label>
-          <label htmlFor="max-price">
-          Max price
-          <input
-          type="range"
-          id="max-price"
-          min="0.5"
-          max="9"
-          step="0.25"
-          value={max}
-          onChange={(event) => setMax(event.target.value)}
-          />
-         </label>
-          </fieldset>
+          <PriceFilter min={min} setMin={setMin} max={max} setMax={setMax}/>
         </form>
       </section>
       <section className="dishes">
         <h2>Dishes</h2>
-        <ul className="grid">
-          {dishes.filter((item) => item.price > min && item.price < max)
-          .map((item) => (
-            <li key={item.id} className="card">
-              <ul><li>{item.name}</li></ul>
-              <ul><li>{item.description}</li></ul>
-              <ul><li>{item.price}</li></ul>
-            </li>
-            ))}
-        </ul>
+        <DishList min={min} max={max} />
       </section>
     </main>
   );
---
+++ Client-Side_W5/react-forms/workshop/DishList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import dishes from "../data";
+
+function DishList(props){
+    return (
+        <ul className="grid">
+            {dishes.filter((item) => item.price > props.min && item.price < props.max)
+          .map((item) => (
+            <li key={item.id} className="card">
+              <ul><li>{item.name}</li></ul>
+              <ul><li>{item.description}</li></ul>
+              <ul><li>{item.price}</li></ul>
+            </li>
+            ))}
+        
+        </ul>
+    )
+}
+
+export default DishList;
