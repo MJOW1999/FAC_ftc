641da56fc24337088aff0ece4bfa61fb1a399b9f
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-26 10:53:23 +0100

    Part 4: Filter list by price

--- Client-Side_W5/react-forms/workshop/App.jsx
+++ Client-Side_W5/react-forms/workshop/App.jsx
@@ -13,20 +13,51 @@ import dishes from "../data";
 // ];
 
 function App() {
+  const [min, setMin] = React.useState(0);
+  const [max, setMax] = React.useState(9);
   return (
     <main>
       <section className="filters">
         <h1>Burger Place</h1>
-        <form>Inputs go here</form>
+        <form>
+          <fieldset>
+          <legend>Price</legend>
+          <label htmlFor="min-price">
+            Min price
+            <input
+          type="range"
+          id="min-price"
+          min="0.5"
+          max="9"
+          step="0.25"
+          value={min}
+          onChange={(event) => setMin(event.target.value)}
+          />
+          </label>
+          <label htmlFor="max-price">
+          Max price
+          <input
+          type="range"
+          id="max-price"
+          min="0.5"
+          max="9"
+          step="0.25"
+          value={max}
+          onChange={(event) => setMax(event.target.value)}
+          />
+         </label>
+          </fieldset>
+        </form>
       </section>
       <section className="dishes">
         <h2>Dishes</h2>
         <ul className="grid">
-          {dishes.map((burger) => (
-          <li key={burger.id} className="card">
-            <ul><li>{burger.name}</li></ul>
-            <ul><li>{burger.description}</li></ul>
-            <ul><li>{burger.price}</li></ul>
+          {dishes.filter((item) => item.price > min && item.price < max)
+          .map((item) => (
+            <li key={item.id} className="card">
+              <ul><li>{item.name}</li></ul>
+              <ul><li>{item.description}</li></ul>
+              <ul><li>{item.price}</li></ul>
             </li>
             ))}
         </ul>
