9f3128394fdd4265354ad887e8193f583a90a694
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-26 16:18:11 +0100

    Challenge 3: searching for users

--- Client-Side_W5/react-fetch/workshop/App.jsx
+++ Client-Side_W5/react-fetch/workshop/App.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import Profile from "./Profile";
 
 function App() {
+  const [user, setUser] = React.useState("mjow1999");
   return (
     <main>
-      <div><Profile name="mjow1999"/></div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setUser(event.target.username.value);
+        }}>
+        <input
+          type="search"
+          aria-label="Search users"
+          placeholder="Search users"
+          name="username"
+        />
+        </form>
+
+      <div><Profile name={user}/></div>
     </main>
   );
 }
