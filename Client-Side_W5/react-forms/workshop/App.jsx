f7b16675893d461f3688858aee22a63ec767fd53
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-26 11:50:04 +0100

    Part 6: Create a radio group

--- Client-Side_W5/react-forms/workshop/App.jsx
+++ Client-Side_W5/react-forms/workshop/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import dishes from "../data";
 import PriceFilter from "./PriceFilter";
+import DishList from "./DishList";
+import CategoryFilter from "./CategoryFilter";
 
 // const categories = [
 //   "all",
@@ -16,12 +18,16 @@ import PriceFilter from "./PriceFilter";
 function App() {
   const [min, setMin] = React.useState(0);
   const [max, setMax] = React.useState(9);
+  const [checked, setChecked] = React.useState(false);
   return (
     <main>
       <section className="filters">
         <h1>Burger Place</h1>
         <form>
           <PriceFilter min={min} setMin={setMin} max={max} setMax={setMax}/>
+        </form>
+        <form>
+          <CategoryFilter checked={checked} setChecked={setChecked} />
         </form>
       </section>
       <section className="dishes">
