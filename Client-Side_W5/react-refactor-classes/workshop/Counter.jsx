e332a3a9ff35a487e8a54772dd3075762a4d0171
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-29 10:48:37 +0100

    React refactor challenges 1 and 2
    Haven't finished the challenges and the test functions didn't work

--- Client-Side_W5/react-refactor-classes/workshop/Counter.jsx
+++ Client-Side_W5/react-refactor-classes/workshop/Counter.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 
-class Counter extends React.Component {
-  state = {
-    count: 0,
-  };
-  decrement = () => {
-    this.setState((oldState) => {
-      return { count: oldState.count - 1 };
-    });
-  };
-  increment = () => {
-    this.setState((oldState) => {
-      return { count: oldState.count + 1 };
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.decrement} aria-label="Decrement count">
+// class Counter extends React.Component {
+//   state = {
+//     count: 0,
+//   };
+//   decrement = () => {
+//     this.setState((oldState) => {
+//       return { count: oldState.count - 1 };
+//     });
+//   };
+//   increment = () => {
+//     this.setState((oldState) => {
+//       return { count: oldState.count + 1 };
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.decrement} aria-label="Decrement count">
+//           -
+//         </button>
+//         <span>Count is {this.state.count}</span>
+//         <button onClick={this.increment} aria-label="Increment count">
+//           +
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+function Counter(){
+  const [count, setCount] = React.useState(0);
+  const decrement = setCount((count) => count - 1);
+  const increment = setCount((count) => count + 1);
+  return (
+    <div>
+        <button onClick={decrement} aria-label="Decrement count">
           -
         </button>
-        <span>Count is {this.state.count}</span>
-        <button onClick={this.increment} aria-label="Increment count">
+        <span>Count is {count}</span>
+        <button onClick={increment} aria-label="Increment count">
           +
         </button>
       </div>
     );
-  }
 }
 
 export default Counter;
