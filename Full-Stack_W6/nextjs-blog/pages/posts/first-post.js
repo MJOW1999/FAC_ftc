221190950c5b80e561c8951532e3fa02fb7abdac
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-01 12:20:56 +0000

    Successfully implemented image and layout on first post route

--- Full-Stack_W6/nextjs-blog/pages/posts/first-post.js
+++ Full-Stack_W6/nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -14,6 +15,12 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </Layout>
   );
 }
