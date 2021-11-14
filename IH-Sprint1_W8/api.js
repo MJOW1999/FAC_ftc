de58c801aa85c5d2279e467c0a6e9b1219278622
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-14 17:15:38 +0000

    Created template for album data and displayed to page

--- IH-Sprint1_W8/api.js
+++ IH-Sprint1_W8/api.js
@@ -7,4 +7,32 @@ fetch(
     }
     return response.json();
   })
-  .then((data) => console.log(data.results.albummatches.album[0]));
+  .then((data) => AlbumData(data.results.albummatches.album));
+
+let arr = [];
+
+async function AlbumData(album) {
+  for (let i = 0; i < 5; i++) {
+    console.log(album[i]);
+    let image = album[i].image[0][`#text`];
+    let name = album[i].name;
+    let artist = album[i].artist;
+    arr.push({ image, name, artist });
+  }
+  console.log(arr);
+  arr.forEach(createCardUsingTemplate);
+}
+
+const results = document.querySelector("#albumResults");
+
+function createCardUsingTemplate(e) {
+  const { image, name, artist } = e;
+  const template = document.querySelector("#template");
+  const domFragment = template.content.cloneNode(true);
+  let card = domFragment.querySelector("#templateCard");
+  card.setAttribute("data-name", `${name}`);
+  domFragment.querySelector("img").src = image;
+  domFragment.querySelector("#album_name").textContent = name;
+  domFragment.querySelector("#album_artist").textContent = artist;
+  results.append(domFragment);
+}
